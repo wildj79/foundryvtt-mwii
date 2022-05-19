058bceedcee039ae4b75806c1991299a36bba6b9
James Allred <wildj79@gmail.com>
2022-05-19 08:22:28 -0500

    Updating development dependencies and setting up the gulpfile as an ESM module.

--- gulpfile.js
+++ gulpfile.js
@@ -1,41 +1,17 @@
-const gulp = require('gulp');
-const fs = require('fs-extra');
-const path = require('path');
-const chalk = require('chalk');
-const archiver = require('archiver');
-const stringify = require('json-stringify-pretty-compact');
-const less = require('gulp-less');
-
-const argv = require('yargs').argv;
-
-function getConfig() {
-	const configPath = path.resolve(process.cwd(), 'foundryconfig.json');
-	let config;
-
-	if (fs.existsSync(configPath)) {
-		config = fs.readJSONSync(configPath);
-		return config;
-	} else {
-		return;
-	}
-}
+import gulp from "gulp";
+import fs from "fs-extra";
+import path from "path";
+import archiver from "archiver";
+import less from "gulp-less";
 
 function getManifest() {
-	const json = {};
+	const json = {
+		root: 'src'
+	};
 
-	if (fs.existsSync('src')) {
-		json.root = 'src';
-	} else {
-		json.root = 'dist';
-	}
-
-	const modulePath = path.join(json.root, 'module.json');
 	const systemPath = path.join(json.root, 'system.json');
 
-	if (fs.existsSync(modulePath)) {
-		json.file = fs.readJSONSync(modulePath);
-		json.name = 'module.json';
-	} else if (fs.existsSync(systemPath)) {
+	if (fs.existsSync(systemPath)) {
 		json.file = fs.readJSONSync(systemPath);
 		json.name = 'system.json';
 	} else {
@@ -115,7 +91,7 @@ function buildWatch() {
 	);
 }
 
-async function copyLocalization() {
+export async function copyLocalization() {
 	console.log("Opening localization files");
 	const englishFilePath = "./src/lang/en.json";
 	const englishRaw = await fs.readFile(englishFilePath);
@@ -171,7 +147,7 @@ function mergeDeep(target, ...sources) {
  * Remove built files from `dist` folder
  * while ignoring source files
  */
-async function clean() {
+export async function clean() {
 	const name = 'mwii';
 	const files = [];
 	
@@ -191,8 +167,8 @@ async function clean() {
 		'LICENSE'
 	);
 
-	console.log(' ', chalk.yellow('Files to clean:'));
-	console.log('   ', chalk.blueBright(files.join('\n    ')));
+	console.log(' ', 'Files to clean:');
+	console.log('   ', files.join('\n    '));
 
 	// Attempt to remove the files
 	try {
@@ -230,9 +206,7 @@ async function linkUserData() {
 			destDir = 'systems';
 		} else {
 			throw Error(
-				`Could not find ${chalk.blueBright(
-					'module.json'
-				)} or ${chalk.blueBright('system.json')}`
+				`Could not find 'module.json' or 'system.json'`
 			);
 		}
 
@@ -246,15 +220,9 @@ async function linkUserData() {
 			throw Error('No User Data path defined in foundryconfig.json');
 		}
 
-		if (argv.clean || argv.c) {
+		if (!fs.existsSync(linkDir)) {
 			console.log(
-				chalk.yellow(`Removing build in ${chalk.blueBright(linkDir)}`)
-			);
-
-			await fs.remove(linkDir);
-		} else if (!fs.existsSync(linkDir)) {
-			console.log(
-				chalk.green(`Copying build to ${chalk.blueBright(linkDir)}`)
+				`Copying build to ${linkDir}`
 			);
 			await fs.symlink(path.resolve('./dist'), linkDir);
 		}
@@ -276,13 +244,6 @@ async function packageBuild() {
 
 	return new Promise((resolve, reject) => {
 		try {
-			// Remove the package dir without doing anything else
-			if (argv.clean || argv.c) {
-				console.log(chalk.yellow('Removing all packaged files'));
-				fs.removeSync('package');
-				return;
-			}
-
 			// Ensure there is a directory to hold all the packaged versions
 			fs.ensureDirSync('package');
 
@@ -292,9 +253,9 @@ async function packageBuild() {
 			const zip = archiver('zip', { zlib: { level: 9 } });
 
 			zipFile.on('close', () => {
-				console.log(chalk.green(zip.pointer() + ' total bytes'));
+				console.log(zip.pointer() + ' total bytes');
 				console.log(
-					chalk.green(`Zip file ${zipName} has been written`)
+					`Zip file ${zipName} has been written`
 				);
 				return resolve();
 			});
@@ -315,121 +276,18 @@ async function packageBuild() {
 	});
 }
 
-/*********************/
-/*		PACKAGE		 */
-/*********************/
-
-/**
- * Update version and URLs in the manifest JSON
- */
-function updateManifest(cb) {
-	const packageJson = fs.readJSONSync('package.json');
-	const config = getConfig(),
-		manifest = getManifest(),
-		rawURL = config.rawURL,
-		repoURL = config.repository,
-		manifestRoot = manifest.root;
-
-	if (!config) cb(Error(chalk.red('foundryconfig.json not found')));
-	if (!manifest) cb(Error(chalk.red('Manifest JSON not found')));
-	if (!rawURL || !repoURL)
-		cb(
-			Error(
-				chalk.red(
-					'Repository URLs not configured in foundryconfig.json'
-				)
-			)
-		);
-
-	try {
-		const version = argv.update || argv.u;
-
-		/* Update version */
-
-		const versionMatch = /^(\d{1,}).(\d{1,}).(\d{1,})$/;
-		const currentVersion = manifest.file.version;
-		let targetVersion = '';
-
-		if (!version) {
-			cb(Error('Missing version number'));
-		}
-
-		if (versionMatch.test(version)) {
-			targetVersion = version;
-		} else {
-			targetVersion = currentVersion.replace(
-				versionMatch,
-				(substring, major, minor, patch) => {
-					console.log(
-						substring,
-						Number(major) + 1,
-						Number(minor) + 1,
-						Number(patch) + 1
-					);
-					if (version === 'major') {
-						return `${Number(major) + 1}.0.0`;
-					} else if (version === 'minor') {
-						return `${major}.${Number(minor) + 1}.0`;
-					} else if (version === 'patch') {
-						return `${major}.${minor}.${Number(patch) + 1}`;
-					} else {
-						return '';
-					}
-				}
-			);
-		}
-
-		if (targetVersion === '') {
-			return cb(Error(chalk.red('Error: Incorrect version arguments.')));
-		}
-
-		if (targetVersion === currentVersion) {
-			return cb(
-				Error(
-					chalk.red(
-						'Error: Target version is identical to current version.'
-					)
-				)
-			);
-		}
-		console.log(`Updating version number to '${targetVersion}'`);
-
-		packageJson.version = targetVersion;
-		manifest.file.version = targetVersion;
-
-		/* Update URLs */
-
-		const result = `${rawURL}/v${manifest.file.version}/package/${manifest.file.name}-v${manifest.file.version}.zip`;
-
-		manifest.file.url = repoURL;
-		manifest.file.manifest = `${rawURL}/master/${manifestRoot}/${manifest.name}`;
-		manifest.file.download = result;
-
-		const prettyProjectJson = stringify(manifest.file, {
-			maxLength: 35,
-			indent: '\t',
-		});
-
-		fs.writeJSONSync('package.json', packageJson, { spaces: '\t' });
-		fs.writeFileSync(
-			path.join(manifest.root, manifest.name),
-			prettyProjectJson,
-			'utf8'
-		);
-
-		return cb();
-	} catch (err) {
-		cb(err);
-	}
-}
-
 const execBuild = gulp.parallel(buildLess, copyFiles);
 
-exports.build = gulp.series(clean, execBuild);
-exports.watch = buildWatch;
-exports.clean = clean;
-exports.link = linkUserData;
-exports.package = gulp.series(copyReadmeAndLicenses, packageBuild);
-exports.update = updateManifest;
-exports.copyLocalization = copyLocalization;
-exports.default = gulp.series(clean, execBuild);
+const build = gulp.series(clean, execBuild);
+const watch = buildWatch;
+const link = linkUserData;
+const _package = gulp.series(copyReadmeAndLicenses, packageBuild);
+const _default = build;
+
+export { 
+	_default as default, 
+	_package as package,
+	build,
+	watch,
+	link
+};
